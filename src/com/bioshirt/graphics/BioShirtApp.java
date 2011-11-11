d2fcf12720db553f5a01231540c733c72f0ba9c1
J Austin Tobin <neophoebus@gmail.com>
2011-11-11 14:03:26 +0000

    Fixed the GUI even more, almost back to previous state

--- src/com/bioshirt/graphics/BioShirtApp.java
+++ src/com/bioshirt/graphics/BioShirtApp.java
@@ -25,18 +25,22 @@ import com.bioshirt.dao.STIDAOImpl;
 import com.bioshirt.dto.Device;
 import com.bioshirt.dto.SensorTimeInstance;
 import com.bioshirt.services.SimpleRead;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class BioShirtApp extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField txtUsername;
 	private JPasswordField pwdPassword;
-	protected static  SimpleRead sr = null;
+//	protected static SimpleRead sr = null;
 	private static final JLabel flexValue = new JLabel("Flex Value");
+	private static final JLabel cardValue = new JLabel("Card Value");
+	private static final JLabel acceValue = new JLabel("Acce Value");
 	private static final Random r = new Random();
-	private static final STIDAO stidao = new STIDAOImpl();
+//	private static final STIDAO stidao = new STIDAOImpl();
 	
-	private static Device device = new Device("TESTDEVICEID");
+//	private static Device device = new Device("TESTDEVICEID");
 
 	/**
 	 * Launch the application.
@@ -57,15 +61,33 @@ public class BioShirtApp extends JFrame {
 					String randomHex = Hex.encodeHexString(bytes);
 					char c = randomHex.charAt(0);
 					randomHex = randomHex.replace(c, '0');
-							
-							
-					System.out.println(randomHex.length());
 					flexValue.setText(randomHex);
-					Timestamp date = new Timestamp(System.currentTimeMillis());
-					stidao.insertSTI(new SensorTimeInstance(device, date, randomHex));
 					Integer value = Integer.decode("0x"+randomHex);
-					System.out.println("random hex " + value);
-					flexValue.setForeground(colorMapper(3210, value ));
+					flexValue.setForeground(colorMapper(2457, value ));
+					
+					//replace next lines with code to get the input from serial io
+					r.nextBytes(bytes);
+					String randomHexTwo = Hex.encodeHexString(bytes);
+					c = randomHexTwo.charAt(0);
+					randomHexTwo = randomHexTwo.replace(c, '0');
+					acceValue.setText(randomHexTwo);
+					value = Integer.decode("0x"+randomHexTwo);
+					acceValue.setForeground(colorMapper(2457, value ));
+					
+					r.nextBytes(bytes);
+					String randomHexThree = Hex.encodeHexString(bytes);
+					c = randomHexThree.charAt(0);
+					randomHexThree = randomHexThree.replace(c, '0');
+					cardValue.setText(randomHexThree);
+					value = Integer.decode("0x"+randomHexThree);
+					cardValue.setForeground(colorMapper(2457, value ));
+					
+					
+					System.out.println("random hex:  " + randomHex + randomHexTwo + randomHexThree);
+					
+//					Timestamp date = new Timestamp(System.currentTimeMillis());
+//					stidao.insertSTI(new SensorTimeInstance(device, date, randomHex));
+
 
 					try {
 						Thread.sleep(2000);
@@ -85,6 +107,7 @@ public class BioShirtApp extends JFrame {
                 int blue = 0;
                 //decrease based on deviation
                 int green = 200;
+                //TODO fix this again
                 
                 if (actual < expected) {
                     blue +=Math.abs(expected - actual);
@@ -162,63 +185,104 @@ public class BioShirtApp extends JFrame {
 		sl_contentPane.putConstraint(SpringLayout.EAST, tabbedPane, 640, SpringLayout.WEST, contentPane);
 		contentPane.add(tabbedPane);
 		
-		JPanel panel = new JPanel();
-		tabbedPane.addTab("Login", null, panel, null);
-		SpringLayout sl_panel = new SpringLayout();
-		panel.setLayout(sl_panel);
+		JPanel loginPanel = new JPanel();
+		tabbedPane.addTab("Login", null, loginPanel, null);
+		SpringLayout sl_loginPanel = new SpringLayout();
+		loginPanel.setLayout(sl_loginPanel);
 		
 		txtUsername = new JTextField();
-		sl_panel.putConstraint(SpringLayout.SOUTH, txtUsername, -261, SpringLayout.SOUTH, panel);
-		sl_panel.putConstraint(SpringLayout.EAST, txtUsername, -246, SpringLayout.EAST, panel);
+		sl_loginPanel.putConstraint(SpringLayout.SOUTH, txtUsername, -261, SpringLayout.SOUTH, loginPanel);
+		sl_loginPanel.putConstraint(SpringLayout.EAST, txtUsername, -246, SpringLayout.EAST, loginPanel);
 		txtUsername.setText("Username");
-		panel.add(txtUsername);
+		loginPanel.add(txtUsername);
 		txtUsername.setColumns(10);
 		
 		pwdPassword = new JPasswordField();
-		sl_panel.putConstraint(SpringLayout.NORTH, pwdPassword, 6, SpringLayout.SOUTH, txtUsername);
-		sl_panel.putConstraint(SpringLayout.WEST, pwdPassword, 0, SpringLayout.WEST, txtUsername);
-		sl_panel.putConstraint(SpringLayout.EAST, pwdPassword, -246, SpringLayout.EAST, panel);
+		sl_loginPanel.putConstraint(SpringLayout.NORTH, pwdPassword, 6, SpringLayout.SOUTH, txtUsername);
+		sl_loginPanel.putConstraint(SpringLayout.WEST, pwdPassword, 0, SpringLayout.WEST, txtUsername);
+		sl_loginPanel.putConstraint(SpringLayout.EAST, pwdPassword, -246, SpringLayout.EAST, loginPanel);
 		pwdPassword.setText("Password");
-		panel.add(pwdPassword);
+		loginPanel.add(pwdPassword);
 		
 		JButton btnLogin = new JButton("Login");
-		sl_panel.putConstraint(SpringLayout.SOUTH, btnLogin, -10, SpringLayout.SOUTH, panel);
-		sl_panel.putConstraint(SpringLayout.EAST, btnLogin, -10, SpringLayout.EAST, panel);
-		panel.add(btnLogin);
+		sl_loginPanel.putConstraint(SpringLayout.SOUTH, btnLogin, -10, SpringLayout.SOUTH, loginPanel);
+		sl_loginPanel.putConstraint(SpringLayout.EAST, btnLogin, -10, SpringLayout.EAST, loginPanel);
+		loginPanel.add(btnLogin);
 		
-		JPanel panel_1 = new JPanel();
-		tabbedPane.addTab("Graph", null, panel_1, null);
-		SpringLayout sl_panel_1 = new SpringLayout();
-		panel_1.setLayout(sl_panel_1);
+		JPanel livePanel = new JPanel();
+		tabbedPane.addTab("Live", null, livePanel, null);
+		SpringLayout sl_livePanel = new SpringLayout();
+		livePanel.setLayout(sl_livePanel);
 		
-		JPanel panel_2 = new JPanel();
-		sl_panel_1.putConstraint(SpringLayout.NORTH, panel_2, 10, SpringLayout.NORTH, panel_1);
-		sl_panel_1.putConstraint(SpringLayout.WEST, panel_2, 44, SpringLayout.WEST, panel_1);
-		sl_panel_1.putConstraint(SpringLayout.SOUTH, panel_2, 327, SpringLayout.NORTH, panel_1);
-		sl_panel_1.putConstraint(SpringLayout.EAST, panel_2, 404, SpringLayout.WEST, panel_1);
-		panel_2.setBackground(Color.WHITE);
-		panel_1.add(panel_2);
-		
-		JComboBox comboBox = new JComboBox();
-		sl_panel_1.putConstraint(SpringLayout.NORTH, comboBox, 35, SpringLayout.NORTH, panel_1);
-		sl_panel_1.putConstraint(SpringLayout.WEST, comboBox, 6, SpringLayout.EAST, panel_2);
-		sl_panel_1.putConstraint(SpringLayout.EAST, comboBox, -10, SpringLayout.EAST, panel_1);
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Accelerometer : All", "Accelerometer : Date", "Cardiogram : All", "Cardiogram : Date", "Flex : All", "Flex : Date"}));
-		panel_1.add(comboBox);
-		
-		JButton btnDate = new JButton("Choose Date");
-		sl_panel_1.putConstraint(SpringLayout.NORTH, btnDate, 6, SpringLayout.SOUTH, comboBox);
-		sl_panel_1.putConstraint(SpringLayout.EAST, btnDate, -10, SpringLayout.EAST, panel_1);
-		panel_1.add(btnDate);
-		
-		JButton btnExecute = new JButton("Execute");
-		sl_panel_1.putConstraint(SpringLayout.SOUTH, btnExecute, 0, SpringLayout.SOUTH, panel_2);
+		JPanel displayPanel = new JPanel();
+		sl_livePanel.putConstraint(SpringLayout.NORTH, displayPanel, 10, SpringLayout.NORTH, livePanel);
+		sl_livePanel.putConstraint(SpringLayout.WEST, displayPanel, 44, SpringLayout.WEST, livePanel);
+		sl_livePanel.putConstraint(SpringLayout.SOUTH, displayPanel, 327, SpringLayout.NORTH, livePanel);
+		sl_livePanel.putConstraint(SpringLayout.EAST, displayPanel, 404, SpringLayout.WEST, livePanel);
+		displayPanel.setBackground(Color.WHITE);
+		livePanel.add(displayPanel);
+		SpringLayout sl_displayPanel = new SpringLayout();
+		displayPanel.setLayout(sl_displayPanel);
 		
 		JLabel lblFlex = new JLabel("Flex");
-		panel_2.add(lblFlex);
-		panel_2.add(flexValue);
-	
-		sl_panel_1.putConstraint(SpringLayout.EAST, btnExecute, -10, SpringLayout.EAST, panel_1);
-		panel_1.add(btnExecute);
+		sl_displayPanel.putConstraint(SpringLayout.NORTH, flexValue, 0, SpringLayout.NORTH, lblFlex);
+		sl_displayPanel.putConstraint(SpringLayout.NORTH, lblFlex, 29, SpringLayout.NORTH, displayPanel);
+		displayPanel.add(lblFlex);
+		displayPanel.add(flexValue);
+		
+		JLabel acceLabel = new JLabel("Acce");
+		sl_displayPanel.putConstraint(SpringLayout.WEST, lblFlex, 0, SpringLayout.WEST, acceLabel);
+		sl_displayPanel.putConstraint(SpringLayout.NORTH, acceLabel, 91, SpringLayout.NORTH, displayPanel);
+		displayPanel.add(acceLabel);
+		
+		
+		sl_displayPanel.putConstraint(SpringLayout.EAST, flexValue, 0, SpringLayout.EAST, acceValue);
+		sl_displayPanel.putConstraint(SpringLayout.NORTH, acceValue, 91, SpringLayout.NORTH, displayPanel);
+		sl_displayPanel.putConstraint(SpringLayout.WEST, acceValue, 163, SpringLayout.WEST, displayPanel);
+		sl_displayPanel.putConstraint(SpringLayout.EAST, acceLabel, -5, SpringLayout.WEST, acceValue);
+		displayPanel.add(acceValue);
+		
+		JLabel cardLabel = new JLabel("Card");
+		sl_displayPanel.putConstraint(SpringLayout.WEST, cardLabel, 0, SpringLayout.WEST, acceLabel);
+		sl_displayPanel.putConstraint(SpringLayout.SOUTH, cardLabel, -137, SpringLayout.SOUTH, displayPanel);
+		displayPanel.add(cardLabel);
+		
+		
+		sl_displayPanel.putConstraint(SpringLayout.NORTH, cardValue, 0, SpringLayout.NORTH, cardLabel);
+		sl_displayPanel.putConstraint(SpringLayout.WEST, cardValue, 6, SpringLayout.EAST, cardLabel);
+		displayPanel.add(cardValue);
+		
+		JPanel graphPanel = new JPanel();
+		tabbedPane.addTab("Graph", null, graphPanel, null);
+		SpringLayout sl_graphPanel = new SpringLayout();
+		graphPanel.setLayout(sl_graphPanel);
+		
+		JPanel graphDisplay = new JPanel();
+		sl_graphPanel.putConstraint(SpringLayout.WEST, graphDisplay, 10, SpringLayout.WEST, graphPanel);
+		sl_graphPanel.putConstraint(SpringLayout.EAST, graphDisplay, 475, SpringLayout.WEST, graphPanel);
+		graphDisplay.setBackground(Color.WHITE);
+		sl_graphPanel.putConstraint(SpringLayout.NORTH, graphDisplay, 10, SpringLayout.NORTH, graphPanel);
+		sl_graphPanel.putConstraint(SpringLayout.SOUTH, graphDisplay, 339, SpringLayout.NORTH, graphPanel);
+		graphPanel.add(graphDisplay);
+		
+		JButton button = new JButton("Choose Date");
+		sl_graphPanel.putConstraint(SpringLayout.EAST, button, -10, SpringLayout.EAST, graphPanel);
+		graphPanel.add(button);
+		
+		JButton button_1 = new JButton("Execute");
+		sl_graphPanel.putConstraint(SpringLayout.NORTH, button_1, 41, SpringLayout.SOUTH, button);
+		sl_graphPanel.putConstraint(SpringLayout.EAST, button_1, -10, SpringLayout.EAST, graphPanel);
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		graphPanel.add(button_1);
+		
+		JComboBox comboBox = new JComboBox();
+		sl_graphPanel.putConstraint(SpringLayout.WEST, comboBox, 20, SpringLayout.EAST, graphDisplay);
+		sl_graphPanel.putConstraint(SpringLayout.EAST, comboBox, -10, SpringLayout.EAST, graphPanel);
+		sl_graphPanel.putConstraint(SpringLayout.NORTH, button, 6, SpringLayout.SOUTH, comboBox);
+		sl_graphPanel.putConstraint(SpringLayout.NORTH, comboBox, 10, SpringLayout.NORTH, graphPanel);
+		graphPanel.add(comboBox);
 	}
 }
